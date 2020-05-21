94a14681de2e44141cf1be738704dd9f2291e52d
jiehaijun <jiehaijun@163.com>
2020-05-21 16:30:21 +0800

    Update login.php
    udpate

--- webroot/html/login.php
+++ webroot/html/login.php
@@ -5,5 +5,5 @@
     $password = $_POST['password'];
     $result = mysqli_query($conn, "SELECT IFNULL((SELECT 'Y' from user where email = '".$email."' AND password = '".$password."' limit 1),'N')");
     $row=mysqli_fetch_row($result);
-    echo $result;
+    echo $row;
 ?>
