d1724fb07eebfce1817b7e0f150fd27c0cc2a5ff
jiehaijun <jiehaijun@163.com>
2020-05-21 15:25:14 +0800

    Update index.php

--- webroot/index.php
+++ webroot/index.php
@@ -1,13 +1,3 @@
-<?php
-use App\Application;
-use Cake\Http\Server;
-
-// Bind your application to the server.
-$server = new Server(new Application(dirname(__DIR__) . '/config'));
-
-// Run the request/response through the application and emit the response.
-$server->emit($server->run());
-?>
 <!doctype html>
 <html>
 
