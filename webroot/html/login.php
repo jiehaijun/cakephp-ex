c7bab4371ab367e9fd95216f564816e10ae077b8
jiehaijun <jiehaijun@163.com>
2020-05-21 16:38:08 +0800

    Update login.php
    update login

--- webroot/html/login.php
+++ webroot/html/login.php
@@ -5,5 +5,13 @@
     $password = $_POST['password'];
     $result = mysqli_query($conn, "SELECT IFNULL((SELECT 'Y' from user where email = '".$email."' AND password = '".$password."' limit 1),'N')");
     $row=mysqli_fetch_row($result);
-    echo $row;
+    echo $row[0];
+    if ($row[0] == 'Y') {
+        $url = "addPost.html?email="+$email;
+    }else{
+        $url = "login.html";
+    }
+    echo "<script type='text/javascript'>";
+    echo "window.location.href='$url'";
+    echo "</script>"; 
 ?>
