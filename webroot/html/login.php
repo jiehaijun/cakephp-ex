2fdf4145b6ceacb561c9e845d62c6eb22683bce1
jiehaijun <jiehaijun@163.com>
2020-05-21 16:26:41 +0800

    Update

--- webroot/html/login.php
+++ webroot/html/login.php
@@ -4,5 +4,6 @@
     $email = $_POST['email'];
     $password = $_POST['password'];
     $result = mysqli_query($conn, "SELECT IFNULL((SELECT 'Y' from user where email = '".$email."' AND password = '".$password."' limit 1),'N')");
+    $row=mysqli_fetch_row($result);
     echo $result;
 ?>
